57c0c70a1f03ab1698baf8692390d4b9119425a8
Adélie Saulé <adelie.saule@etu.minesparis.psl.eu>
2023-11-28 10:34:07 +0100

    suppression verres avec oxydes sous représentés

--- MIG_Nettoyage_dataframe.py
+++ MIG_Nettoyage_dataframe.py
@@ -22,7 +22,24 @@ n = len(df)
 for x in df.columns[0]:
     L.append( (x, df[x][df[x] > 0.1].count()/n) )
 
+def coupe_liste_triee(L, a):
+    Oxydes_gardes = []
+    Oxydes_sortis = []
+    for i in range(len(L)):
+        if L[i][1] < a:
+            Oxydes_gardes = L[:i]
+            Oxydes_sortis = L[i:]
+            return Oxydes_gardes, Oxydes_sortis
 
+#liste d'oxydes de Raviner 2020 pour comparaison
+#  SiO2, B2O3, Al2O3, MgO, CaO, BaO, Li2O,
+# Na2O, K2O, Ag2O, Cs2O, Tl2O, BeO, NiO, CuO, ZnO, CdO, PbO,
+# Ga2O3, Y2O3, La2O3, Gd2O3, Bi2O3, TiO2, ZrO2, TeO2, P2O5, V2O5,
+# Nb2O5, Ta2O5, MoO3, WO3, H2O, Sm2O3, MgF2, PbF2, PbCl2
 
+for (x,i) in Oxydes_sortis[]
+    df.drop(x, axis = 1, inplace=True)
 
+df['sum_check'] = np.where(98 < df['sum'] < 100, True, False)
 
+df.drop(df[df['sum_check']==False].index, inplace=True)
