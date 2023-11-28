2f07aca9f5e0f3bf7a28103b27d8a9279a88662a
Valentin Deumier <valentin.deumier@etu.minesparis.psl.eu>
2023-11-28 10:13:10 +0100

    Convertit les colonnes en float, fonctions qui gardent Young/Densité

--- MIG_Nettoyage_dataframe.py
+++ MIG_Nettoyage_dataframe.py
@@ -2,16 +2,31 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('D:/Users/Valentin/Desktop/1A Mines/Semestre 1/UE15 - MIG/Dataframe_test.csv', index_col='No.')
+df = pd.read_csv('D:/Users/Valentin/Desktop/1A Mines/Semestre 1/UE15 - MIG/mini-projet-info-mig/Dataframe_test.csv', index_col='No.')
 
 liste_col_a_enlever = [' Glass No.', ' Data Source', ' Year', ' Data Source Number']
 liste_composants = [' SiO2', ' B2O3', ' Al2O3', ' CaO', ' Na2O']
 
 df.drop(liste_col_a_enlever, axis = 1, inplace=True)        # Enlève les colonnes inutiles
 
-pd.to_numeric(df, )
+df = df.replace(' *', np.nan)               # * -> NaN
+df = df.replace(' ', np.nan)                # ' ' -> NaN
+df = df.fillna(0)                           # NaN -> 0
+
+df = df.astype(float)                       # type object -> type float
 
 df_composants = df[liste_composants]
-df['Sum'] = df_composants.sum(axis=1)                       # Crée une nouvelle colonne pour connaitre la somme des % de composition
+df['Sum'] = df_composants.sum(axis=1)       # Crée une nouvelle colonne pour connaitre la somme des % de composition
 
-print(df.head(5))
+
+def garde_Young(df):
+    """
+    Méthode qui supprime les lignes où le module d'Young à temp. ambiante n'est pas renseigné
+    """
+    df.dropna(subset=[" Young's Modulus at RT ( GPa )"])
+
+def garde_densité(df):
+    """
+    Méthode qui supprime les lignes où la densité à temp. ambiante n'est pas renseignée
+    """
+    df.dropna(subset=[' Density at RT ( g/cm3 )'])
