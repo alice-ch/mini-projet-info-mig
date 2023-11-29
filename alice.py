227215c604de787a3f7fd3d88e447b919ee4c08f
Raphael-poux <144114417+Raphael-poux@users.noreply.github.com>
2023-11-29 16:25:53 +0100

    Update alice.py

--- alice.py
+++ alice.py
@@ -8,7 +8,7 @@ df = pd.read_csv("C:/Users/raphael.poux/Mig verre/Interglad_propre.csv")
 df = df[df['Glass No.'].str.contains('G')]
 df.drop(['Organic Compound', 'Others', 'O'], axis = 1, inplace = True)
 
-
+# On établit une liste des composants à laquelle on ajoute les propriétés physiques pour créer la liste des colonnes conservées
 Liste_tous_composants = []
 for colonne in df.columns:
     if "O" in colonne:
@@ -20,11 +20,10 @@ Colonnes_gardées.append("Young's Modulus at RT ( GPa )")
 Colonnes_gardées.append('Density at RT ( g/cm3 )')
 Colonnes_gardées.append('Fracture Toughness ( MPa.m1/2 )')
 
-#print(Colonnes_gardées)
-
 df = df[Colonnes_gardées]
 
 # On convertit tout en float
+
 df = df.replace(' *', np.nan)               # * -> NaN
 df = df.replace(' ', np.nan)                # ' ' -> NaN
 df = df.fillna(0)                           # NaN -> 0
@@ -42,18 +41,11 @@ df = df[df['SiO2'] > 0]                     # on ne garde que les verres silicat
 df.drop_duplicates(keep='first', inplace=True)                         # Les duplicatas parfaits
 df.drop_duplicates(subset=Liste_tous_composants, keep=False, inplace=True)       # Les compos identiques
 
-# selection d'oxydes
+# selection d'oxydes dont la composition moyenne dans les oxydes est supérieure à p
 
 def Ox_garde_Gp(df, p):
     D = df[Liste_tous_composants].mean().sort_values( ascending = False)
     return ([ (elem, D[elem]) for elem in D.index if D[elem] > p] , [ (elem, D[elem]) for elem in D.index if D[elem] <= p])
-
-def Ox_garde_Gr(df, r):
-    D = df[Liste_tous_composants].mean()
-    for elem in Liste_tous_composants:
-        D[elem] = df[elem][df[elem] > 0].count()
-    return [ (elem, D[elem]/len(df)*100) for elem in D.sort_values(ascending = False).index if D[elem]/len(df)*100 > r]
-
 
 def supp(df, p):
     oxydes_sortis = [ elem[0] for elem in Ox_garde_Gp(df, p)[1]]
@@ -65,6 +57,14 @@ def supp(df, p):
     df = df[(df['Sum'] > 98) & (df['Sum'] <= 100)]
     return df[oxydes_gardes]
 
+# Méthode alternative dont on est sûrs qu'elle ne fonctionne pas pour l'instant
+
+"""def Ox_garde_Gr(df, r):
+    D = df[Liste_tous_composants].mean()
+    for elem in Liste_tous_composants:
+        D[elem] = df[elem][df[elem] > 0].count()
+    return [ (elem, D[elem]/len(df)*100) for elem in D.sort_values(ascending = False).index if D[elem]/len(df)*100 > r]
+
 def supr(df, p):
     oxydes_sortis = [ elem[0] for elem in Ox_garde_Gr(df, p)[1]]
     oxydes_gardes = [ elem[0] for elem in Ox_garde_Gr(df, p)[0]]
@@ -73,14 +73,15 @@ def supr(df, p):
     df_composants = df[[nom[0] for nom in oxydes_gardes]]
     df['Sum'] = df_composants.sum(axis=1)
     df = df[(df['Sum'] > 98) & (df['Sum'] <= 100)]
-    return df[oxydes_gardes]
+    return df[oxydes_gardes]"""
 
 def Gp2(df, p):
     D = supp(df, p).mean().sort_values( ascending = False)
     return [ elem for elem in D.index if D[elem] > p]
 
-print("i")
+# Le nom des variables se raccourcit car on commence à avoir la flemme
 
+# On reforme une liste de colonnes gardées avec que les oxydes gardés
 
 oxgard = Gp2(df, p)
 cogardees = oxgard[:]
@@ -88,8 +89,8 @@ cogardees.append('Vickers Hardness 100g ( MPa )')
 cogardees.append("Young's Modulus at RT ( GPa )")
 cogardees.append('Density at RT ( g/cm3 )')
 cogardees.append('Fracture Toughness ( MPa.m1/2 )')
-print("i")
 
+# On fait le tri des valeurs aberrantes
 
 df = df[cogardees]
 
@@ -105,7 +106,7 @@ dfD = df.loc[df['Density at RT_check ( g/cm3 )'] == True]
 df['Fracture Toughness_check ( MPa.m1/2 )'] = (df["Fracture Toughness ( MPa.m1/2 )"] > 0.5) & (df["Fracture Toughness ( MPa.m1/2 )"] <= 1.5)
 dfT = df.loc[df['Fracture Toughness_check ( MPa.m1/2 )'] == True]
 
-print("i")
+# On écrit les dataframes dans des fichiers
 
 dfT[oxgard].to_csv('compoT.csv', sep = ' ')
 dfT["Fracture Toughness ( MPa.m1/2 )"].to_csv('T.csv', sep = ' ')
