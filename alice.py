297a458d7191d919831454e43795f30f843d1e41
Raphael-poux <144114417+Raphael-poux@users.noreply.github.com>
2023-11-29 12:19:41 +0100

    Update alice.py

--- alice.py
+++ alice.py
@@ -41,85 +41,28 @@ Liste_tous_composants.remove('O')
 Liste_tous_composants.remove('Organic Compound')
 Liste_tous_composants.remove('Others')
 
-# tri de la liste pour avoir une liste de couples (oxydes, pourcentages de présence) avec un pourcentage de présence décroissant
-
-def fusion(liste1,liste2):
-    liste=[]
-    i,j=0,0
-    while i<len(liste1)and j<len(liste2):
-        if liste1[i][1]>=liste2[j][1]:
-            liste.append(liste1[i])
-            i+=1
-        else:
-            liste.append(liste2[j])
-            j+=1
-    while i<len(liste1):
-        liste.append(liste1[i])
-        i+=1
-    while j<len(liste2):
-        liste.append(liste2[j])
-        j+=1
-    return liste
-
-
-def tri_fusion(liste):
-    if len(liste)<2:
-        return liste[:]
-    else:
-        milieu=len(liste)//2
-        liste1=tri_fusion(liste[:milieu])
-        liste2=tri_fusion(liste[milieu:])
-        return fusion(liste1,liste2)
-
 # On drop les duplicatas
 
 df.drop_duplicates(keep='first', inplace=True)                         # Les duplicatas parfaits
 df.drop_duplicates(subset=Liste_tous_composants, keep=False, inplace=True)       # Les compos identiques
 
-# selection d'oxydes premier tour
-Liste_oxyde_proportion = []
-n = len(df)
-for x in Liste_tous_composants:
-    Liste_oxyde_proportion.append( (x, df[x][df[x] > 0.001].count()/n) )
-print(tri_fusion(Liste_oxyde_proportion))
+# selection d'oxydes
 
-def coupe_liste_triee(L, proportion_apparition_minimum):
-    Oxydes_gardes = []
-    Oxydes_sortis = []
-    for i in range(len(L)):
-        if L[i][1] < proportion_apparition_minimum:
-            Oxydes_gardes = L[:i]
-            Oxydes_sortis = L[i:]
-            return Oxydes_gardes, Oxydes_sortis
-    return L, []
+def Ox_garde_Gp(df, p):
+    D = df[Liste_tous_composants].mean().sort_values( ascending = False)
+    D.head()
+    return ([ (elem, D[elem]) for elem in D.index if D[elem] > p], [ (elem, D[elem]) for elem in D.index if D[elem] =< p])
 
-Oxydes_gardes,Oxydes_sortis = coupe_liste_triee(tri_fusion(Liste_oxyde_proportion), 0.01)
+def Ox_garde_Gr(df, r):
+    for elem in Liste_tous_composants:
+        D[elem] = df[elem][df[elem] > 0].count()
+    return [ (elem, D[elem]/len(df)*100) for elem in D.sort_values(ascending = False).index if D[elem]/len(df)*100 > r])
 
-for (x,i) in Oxydes_sortis:
+def suppression(ox_gardé, df):
+  for (x,i) in Oxydes_sortis:
     df.drop(x, axis = 1, inplace=True)
 
-
-# selection d'oxydes second tour
-Liste_oxyde_proportion = []
-n = len(df)
-for (x,i) in Oxydes_gardes:
-    Liste_oxyde_proportion.append( (x, df[x][df[x] > 0.001].count()/n) )
-print(tri_fusion(Liste_oxyde_proportion))
-
-def coupe_liste_triee(L, proportion_apparition_minimum):
-    Oxydes_gardes = []
-    Oxydes_sortis = []
-    for i in range(len(L)):
-        if L[i][1] < proportion_apparition_minimum:
-            Oxydes_gardes = L[:i]
-            Oxydes_sortis = L[i:]
-            return Oxydes_gardes, Oxydes_sortis
-    return L, []
-
-Oxydes_sortis = coupe_liste_triee(tri_fusion(Liste_oxyde_proportion), 0.01)[1]
-
-for (x,i) in Oxydes_sortis:
-    df.drop(x, axis = 1, inplace=True)
+Oxydes_gardé = Ox_garde_Gp(suppression(Ox_garde_Gp(df, p)[1], df), p)
 
 
 
